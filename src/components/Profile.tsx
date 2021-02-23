82e9ff5dd2c347584e90e0ecf37ff7b4d722e997
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2021-02-23 13:42:52 -0300

    configuring the project to next.js and pushing first components and styles

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import { Profiler } from "inspector";
+
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/evnrodr.png" alt="Evandro Rodrigues" />
+
+            <div>
+                <strong>Evandro Rodrigues</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}
